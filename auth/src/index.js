4c4ec9440ea2610a741cc64ef2975470497c81b3
Vitaliy Semenov <synthex@bk.ru>
2022-07-30 21:43:44 +0300

    auth service is ready

---
+++ auth/src/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const { port, host, db } = require('./configuration');
+const { connectDb } = require('./helpers/db');
+const app = express();
+
+
+const startServer = () => {
+    app.listen(port, () => {
+        console.log(`Started auth service on port ${port}`);
+        console.log(`On host ${host}`);
+        console.log(`Our database ${db}`);
+    })
+};
+
+app.get('/test', (req, res) => {
+    res.send('Our auth server is working correctly');
+});
+
+connectDb()
+    .on('error', console.error.bind(console, 'connection error:'))
+    .on('disconnected', connectDb)
+    .once('open', startServer);
